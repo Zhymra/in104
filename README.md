7395987efeebe32221866cc25730dc14dfb19d03
Zhymra <pl.zhymra@orange.fr>
2022-04-12 15:44:47 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# in104
-seggs
+# CMaze
---
+++ qlearning.c
@@ -0,0 +1,9 @@
+double* Q() {
+	double Q[12][20];
+		for (int i=0;i<12;++i) {
+			for (int j=0;j<20;++j) {
+				Q[i][j]=0;
+				//e greedy a faire a part autre part pour choose
+			}
+		}
+    }
