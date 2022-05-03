72b643029d36c278543c6fe4a6ab53ac94c150b1
Etienne <90619455+PoulpobossMG@users.noreply.github.com>
2022-05-03 16:42:05 +0200

    Update qlearning.c

--- qlearning.c
+++ qlearning.c
@@ -1,4 +1,19 @@
 #include "mazeEnv.h"
+#include "functions.h"
+
+"placeholders - à compléter"
+double alpha = 0.5;
+double gamma =1;
+
+double max_Q(double*** Q, int ligne, int colonne){
+	double max=-100;
+	for (int i=0;i<4;++i){
+		if (Q[ligne][colonne][i]>max){
+			max = Q[ligne][colonne][i];
+		}
+	}
+	return (max);
+}
 
 double* Q() {
 	double Q[rows][cols][4];
@@ -11,15 +26,27 @@ double* Q() {
 		}
 	int etat_ligne=start_row;
 	int etat_col=start_col;
-	int i =0;
+	int ancienne_ligne;
+	int ancienne_col;
 	action prochaine_action;
-	envoutput a;
-	while (etat_ligne!=goal_row || etat_col!=goal_col && i<100) {
-		a = maze_step(prochaine_action);
-		etat_ligne=a.new_row;
-		etat_col=a.new_col;
-		prochaine_action=epsilon_greedy(etat_ligne,etat_col,Q);
-		Q[etat,tab[2],
+	envoutput nouvel_etat;
+	int i =0;
+	while (nouvel_etat.done!=1 && i<100) {
+		"placeholder - à compléter"
+		prochaine_action = right;
+		/* prochaine_action=epsilon_greedy(etat_ligne,etat_col,Q); */
+		nouvel_etat = maze_step(prochaine_action);
+		
+		/* on update les anciennes valeurs */
+		ancienne_ligne=etat_ligne;
+		ancienne_col=etat_col;
+		
+		/* on update les nouvelles valeurs */
+		etat_ligne=nouvel_etat.new_row;
+		etat_col=nouvel_etat.new_col;
+
+		/* on update Q */
+		Q[ancienne_ligne,ancienne_col,prochaine_action] = Q[ancienne_ligne,ancienne_col,prochaine_action] + alpha*(nouvel_etat.reward + gamma*max_Q(Q,etat_ligne,etat_col) - Q[ancienne_ligne,ancienne_col,prochaine_action])
 		
 	}
     }
