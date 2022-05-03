27d17b5b8b993ed43b92cbff3f042d5cd1b6fdd0
Etienne <90619455+PoulpobossMG@users.noreply.github.com>
2022-05-03 15:43:02 +0200

    Update qlearning.c

--- qlearning.c
+++ qlearning.c
@@ -1,22 +1,25 @@
+#include "mazeEnv.h"
+
 double* Q() {
-	double Q[12][20][4];
-		for (int i=0;i<12;++i) {
-			for (int j=0;j<20;++j) {
+	double Q[rows][cols][4];
+		for (int i=0;i<rows;++i) {
+			for (int j=0;j<cols;++j) {
 				for (int k=0;k<4;++k) {
 					Q[i][j][k]=0;
 				}
 			}
 		}
-	s.ligne=8; "on voulait faire un header pour ca"
-	s.col=4;
+	int etat_ligne=start_row;
+	int etat_col=start_col;
 	int i =0;
-	while (s.ligne!=8 || s.col!=15 && i<100) {
-		int tab[3]; "on y mettra l'action voulue et la case s' choisie ainsi"
-		tab[1]=0;
-		tab[2]=0;
-		tab[3]=0;
-		"tab=eg(s.ligne,s.col)"
-		Q[tab[1],tab[2],
+	action prochaine_action;
+	envoutput a;
+	while (etat_ligne!=goal_row || etat_col!=goal_col && i<100) {
+		a = maze_step(prochaine_action);
+		etat_ligne=a.new_row;
+		etat_col=a.new_col;
+		prochaine_action=epsilon_greedy(etat_ligne,etat_col,Q);
+		Q[etat,tab[2],
 		
 	}
     }
