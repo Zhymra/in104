96049a8d78cacfdee1415b27679a88f138488932
Zhymra <pl.zhymra@orange.fr>
2022-05-12 21:12:39 +0200

    Update qlearning_pl.c

--- qlearning_pl.c
+++ qlearning_pl.c
@@ -6,16 +6,16 @@
 
 
 /*	/!\ placeholders - à compléter /!\	*/
-double alpha = 0.5;
+double alpha = 0.9;
 double gamma_temp =1;
-double epsilon=0.15;
+double epsilon=0.1;
 
 double max_Q(double*** Q, int ligne, int colonne){
 	double max=-100;
 	for (int i=0;i<4;++i){
 		if (Q[ligne][colonne][i]>max){
 			max = Q[ligne][colonne][i];
-		}
+		}	
 	}
 	return (max);
 }
@@ -27,15 +27,19 @@ action epsi_greed(double*** Q, int ligne, int colonne){
     action_choisie = env_action_sample();
   } 
   else {
-    double max=-100;
-    int ind_max = 0;
+    int max=-100;
+    int ind_max = env_action_sample();
 	  for (int i=0;i<4;++i){
+	  	//printf("Q possibles : %f", Q[ligne][colonne][i]);
 		  if (Q[ligne][colonne][i]>max){
 			  max = Q[ligne][colonne][i];
+			  
         ind_max = i;
 	 	  }
+	 	  //printf("\n");
 	  }
   action_choisie = ind_max;
+  //printf("random choisi si boucle : %d\n", action_choisie);
   } 
   return(action_choisie);
 }
@@ -44,7 +48,7 @@ action epsi_greed(double*** Q, int ligne, int colonne){
 
 void Q() {
 	
-   double*** Q = (double***)malloc(rows * sizeof(double**));
+   double*** Q = (double***)malloc((rows + 1) * sizeof(double**));
  
     if (Q == NULL)
     {
@@ -52,9 +56,9 @@ void Q() {
         exit(0);
     }
  
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < rows + 1; i++)
     {
-        Q[i] = (double**)malloc(cols * sizeof(double*));
+        Q[i] = (double**)malloc((cols +1) * sizeof(double*));
  
         if (Q[i] == NULL)
         {
@@ -62,9 +66,9 @@ void Q() {
             exit(0);
         }
  
-        for (int j = 0; j < cols; j++)
+        for (int j = 0; j < cols + 1; j++)
         {
-            Q[i][j] = (double*)malloc(4 * sizeof(double));
+            Q[i][j] = (double*)malloc(5 * sizeof(double));
             if (Q[i][j] == NULL)
             {
                 fprintf(stderr, "Out of memory");
@@ -74,52 +78,82 @@ void Q() {
     }
  
     // assign values to the allocated memory
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < rows +1; i++)
     {
-        for (int j = 0; j < cols; j++)
+        for (int j = 0; j < cols + 1; j++)
         {
-            for (int k = 0; k < 4; k++) {
-                Q[i][j][k] = 0;
+            for (int k = 0; k < 5; k++) {
+                Q[i][j][k] = -0.1;
             }
         }
     }	
 
 
-		int etat_ligne=start_row;
-		int etat_col=start_col;
 		int ancienne_ligne;
 		int ancienne_col;
 		action prochaine_action;
 		envOutput nouvel_etat;
 		srand(time(NULL));
+		int i;
 
-	for (int u=0;u<30;++u) { /*ca core dump des qu'on depasse 2 ou 3 */
-		int i =0;
+	for (int u=0;u<300;++u) { 
+		i = 0;
 		nouvel_etat.done=0;
-		while (nouvel_etat.done!=1 && i<1000) {
+		state_row=start_row;
+		state_col=start_col;
+		while (nouvel_etat.done!=1 && i<500) {
+			//printf("%d , %d et ensuite l'action : ", state_row, state_col);
 			
-
-			prochaine_action=epsi_greed(Q,etat_ligne,etat_col); 
+			ancienne_ligne=state_row;
+			ancienne_col=state_col;
+			prochaine_action=epsi_greed(Q,state_row,state_col); 
+			//printf("%d qui donne : ", prochaine_action);
 		
 			nouvel_etat = maze_step(prochaine_action);
 			
 			/* on update les anciennes valeurs */
-			ancienne_ligne=etat_ligne;
-			ancienne_col=etat_col;
+
 			
 			/* on update les nouvelles valeurs */
-			etat_ligne=nouvel_etat.new_row;
-			etat_col=nouvel_etat.new_col;
+
+			//printf("%d , %d\n", state_row, state_col);
 
 			/* on update Q */
-			Q[ancienne_ligne][ancienne_col][prochaine_action] = Q[ancienne_ligne][ancienne_col][prochaine_action] + alpha*(nouvel_etat.reward + gamma_temp*max_Q(Q,etat_ligne,etat_col) - Q[ancienne_ligne][ancienne_col][prochaine_action]);
+			Q[ancienne_ligne][ancienne_col][prochaine_action] = Q[ancienne_ligne][ancienne_col][prochaine_action] +
+			alpha*(nouvel_etat.reward + gamma_temp*max_Q(Q,state_row,state_col) - 
+			Q[ancienne_ligne][ancienne_col][prochaine_action]) - 0.6;
 			i=i+1;
 		}
 		if (nouvel_etat.done==1) {
-			printf("%d Tu es magnifique",i);
+			printf("Tu es magnifique %d \n",i);
 		}
 		else {
-			printf("Tu es une merde");
+			printf("Tu es une merde\n");
 		}
 	}
+  /* print le tableau
+  for (int i = 0; i < rows + 1; i++)
+    {
+        for (int j = 0; j < cols + 1; j++)
+        {
+            for (int k = 0; k < 5; k++) {
+                printf("%f ", Q[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
     }
+   */
+	  
+  for (int i = 0; i < rows + 1; i++)
+    {
+        for (int j = 0; j < cols + 1; j++)
+        {
+           
+                printf("%f ", max_Q(Q,i,j));
+           
+        }
+        printf("\n");
+    }
+   
+}
