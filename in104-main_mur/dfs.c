85e8f627b7ef5adeccb87cde6d1cc557bf183041
Zhymra <pl.zhymra@orange.fr>
2022-05-23 22:37:29 +0200

    Update dfs.c
    commentary

--- in104-main_mur/dfs.c
+++ in104-main_mur/dfs.c
@@ -50,7 +50,11 @@ int main(){
    dfs(start_row,start_col);
    add_crumbs();
    maze_render();
+    
+   /*On lance la fonction qui renverra notre table de Q dans le terminal*/ 
    tableau();
+    
+   /*on free tous les tableaux utilisés*/ 
    free(Q);
    free(maze);
    free(visited);
