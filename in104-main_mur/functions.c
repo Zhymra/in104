98c9c42d252b18d663e75eec4b8ddf86e8d0de36
Zhymra <pl.zhymra@orange.fr>
2022-05-23 22:38:28 +0200

    Update functions.c
    commentary

--- in104-main_mur/functions.c
+++ in104-main_mur/functions.c
@@ -4,10 +4,12 @@ int min(int a, int b){
     return (a>b) ? b : a;
 }
 
+int max(int a, int b){
+    return (a>b) ? a : b;
+}
+
+/*une fonction de max utilisable pour des doubles*/
 double maxd(double a, double b){
     return (a>b) ? a : b;
 }
 
-int max(int a, int b){
-    return (a>b) ? a : b;
-}
